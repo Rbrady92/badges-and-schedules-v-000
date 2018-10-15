9f341b02537074122b55ecb3587e96779ab029f9
Ryan Brady <ryanbrady369@gmail.com>
2018-10-15 23:14:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,17 +1,3 @@
-# Write your code here.
-
-what_do_you_want = [
-                "Hello, my name is Edsger.",
-                "Hello, my name is Ada.",
-                "Hello, my name is Charles.",
-                "Hello, my name is Alan.",
-                "Hello, my name is Grace.",
-                "Hello, my name is Linus.",
-                "Hello, my name is Matz."
-             ]
-
-
-
 
 def badge_maker(name)
   "Hello, my name is #{name}."
